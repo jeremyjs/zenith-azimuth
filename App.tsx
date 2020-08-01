5b6e81b70194e5dda520bab936566a47e8e5c4bc
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2020-07-31 18:04:56 -0700

    Setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
+import { Inbox } from './src/views';
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Inbox />
     </View>
   );
 }
